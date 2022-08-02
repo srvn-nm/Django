7688397e646385208fabad971cd5b037e0a7e3c3
Sarvin Nami <srvn0nm@gmail.com>
2022-08-02 22:24:14 +0430

    adding more views

--- second/home/urls.py
+++ second/home/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from . import views
 urlpatterns=[
-    path('', views.home)
+    path('', views.home),
+    path('contact us', views.contact_us),
 ]
--- second/home/views.py
+++ second/home/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render, HttpResponse
 # Create your views here.
 def home(request):
     return HttpResponse('Hi. This is my home page.')
+
+def contact_us(request):
+    return HttpResponse('You can email me ^-^')
