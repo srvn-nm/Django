18dbbf5afe341a9179342d37da1b0015cf06da9d
Sarvin Nami <srvn0nm@gmail.com>
2022-08-03 00:26:46 +0430

    adding views to the created app

--- second/accounts_app/views.py
+++ second/accounts_app/views.py
@@ -1,3 +1,6 @@
+from django.urls import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+def Sarvin(request):
+    return HttpResponse('This is Sarvin\'s profile page.')
