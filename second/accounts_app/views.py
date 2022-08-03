8215e190643c3d48e897002db98dfcadabdfd286
Sarvin Nami <srvn0nm@gmail.com>
2022-08-03 14:05:45 +0430

    checking validation of the given username

--- second/accounts_app/views.py
+++ second/accounts_app/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render, HttpResponse
 
+users = ['Sarvin', 'Abtin', 'Nazanin', 'Mozhgan', 'Bahram']
+
 # Create your views here.
 def Profile(request, username):
-    return HttpResponse(f'This is {username}\'s profile page.')
+    if username in users:
+        return HttpResponse(f'This is {username}\'s profile page. ^-^')
+    else:
+        return HttpResponse(f'{username} is not a valid username. >-<')
