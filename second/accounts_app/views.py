d5743f909efb1eeae3834b795e869f0794ed5cf0
Sarvin Nami <srvn0nm@gmail.com>
2022-08-03 14:24:36 +0430

    using render

--- second/accounts_app/views.py
+++ second/accounts_app/views.py
@@ -7,7 +7,8 @@ blockedUsers = ['Sarvin']
 # Create your views here.
 def Profile(request, username):
     if username in users and username in blockedUsers:
-        return HttpResponse(f'This  {username} is blocked. >-<')
+        # return HttpResponse(f'This {username} is blocked. >-<')
+        return render(request, 'accounts_app/profile.html')
     elif username in users:
         return HttpResponse(f'This is {username}\'s profile page. ^-^')
     elif username not in users:
