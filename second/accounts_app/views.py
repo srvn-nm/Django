769c0962fcc119c0fca418a6ea195b0738306fe7
Sarvin Nami <srvn0nm@gmail.com>
2022-08-03 14:11:35 +0430

    adding new lists to check usernames

--- second/accounts_app/views.py
+++ second/accounts_app/views.py
@@ -2,9 +2,13 @@ from django.shortcuts import render, HttpResponse
 
 users = ['Sarvin', 'Abtin', 'Nazanin', 'Mozhgan', 'Bahram']
 
+blockedUsers = ['Sarvin']
+
 # Create your views here.
 def Profile(request, username):
-    if username in users:
+    if username in users and username in blockedUsers:
+        return HttpResponse(f'This  {username} is blocked. >-<')
+    elif username in users:
         return HttpResponse(f'This is {username}\'s profile page. ^-^')
-    else:
+    elif username not in users:
         return HttpResponse(f'{username} is not a valid username. >-<')
